5372ccd8d03db4728c7128a65d9bc40c66e1ebc0
WangYifei-0511 <144106221+WangYifei-0511@users.noreply.github.com>
2024-03-17 00:10:38 +0800

    Update inference.py

--- dssi-py-main/src/inference.py
+++ dssi-py-main/src/inference.py
@@ -4,8 +4,8 @@ import numpy as np
 from .data_processor import log_txf, remap_emp_length
 
 def get_prediction(**kwargs):
-    clf = load('models/mdl.joblib')
-    features = load('models/raw_features.joblib')
+    clf = load('mdl.joblib')
+    features = load('raw_features.joblib')
     pred_df = pd.DataFrame(kwargs, index=[0])
     pred_df = log_txf(pred_df, ['annual_inc'])
     pred_df['emp_len'] = pred_df['emp_length'].map(remap_emp_length)
